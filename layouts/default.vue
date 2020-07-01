00b411986d3e65b69c4233dcdf1870cff8200756
ZeffK <54438151+ZeffK@users.noreply.github.com>
2020-07-01 14:08:26 +0530

    yes

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+ <v-app-bar>
+   <v-spacer />
+   <div v-if="$auth.loggedIn">
+     <v-btn text>Logout</v-btn>
+   </div>
+
+   <div v-else>
+     {{ $auth.user }}
+     <v-btn text to="/login">Login</v-btn>
+     <v-btn text to="/register">Register</v-btn>
+     
+   </div>
+ </v-app-bar>
+
+ <v-content>
+
+   <nuxt/>
+ </v-content>
+</div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+     <h1> Login </h1>
+    <UserAuthForm buttonText="Login" :submitForm="loginUser" />
+  </v-container>
+</template>
+
+<script>
+import UserAuthForm from '@/components/UserAuthForm'
+export default {
+    components:{
+        UserAuthForm
+    },
+
+    methods:{
+        loginUser(userinfo){
+            debugger
+            alert('You Pressed Button')
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
